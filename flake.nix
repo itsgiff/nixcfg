865f35f5c285572a491208666abf717a5bd898d9
Paul Gifford <paul@giff.ca>
2025-04-26 15:41:16 -0700

    feat: Add initial flake.nix and macOS placeholder

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
-# ~/.nixcfg/flake.nix
 {
-  description = "Unified Nix configurations for x1 (NixOS) and macbook (macOS)";
+  description = "My unified Nix configurations for x1 (NixOS) and macbook (macOS)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -14,7 +13,6 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Add sops-nix or agenix later if needed
   };
 
   outputs = { self, nixpkgs, home-manager, nix-darwin, ... }@inputs:
@@ -23,49 +21,33 @@
         x1 = { hostname = "x1"; system = "x86_64-linux"; };
         macbook = { hostname = "macbook"; system = "aarch64-darwin"; };
       };
-
-      mkSystem = { hostname, system, pkgs, modules }:
-      if pkgs.stdenv.isLinux then
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs hostname; };
-          modules = modules ++ [
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.paul = import ./users/paul/home.nix;
-            }
-          ];
-        }
-      else if pkgs.stdenv.isDarwin then
-        nix-darwin.lib.darwinSystem {
-          inherit system;
-          specialArgs = { inherit inputs hostname; };
-          modules = modules ++ [
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.paul = import ./users/paul/home.nix;
-            }
-          ];
-        }
-      else
-        throw "Unsupported system: ${system}";
     in {
-      nixosConfigurations.x1 = mkSystem {
-        hostname = systems.x1.hostname;
+      nixosConfigurations.x1 = nixpkgs.lib.nixosSystem {
         system = systems.x1.system;
-        pkgs = nixpkgs.legacyPackages.${systems.x1.system};
-        modules = [ ./hosts/x1/configuration.nix ];
+        specialArgs = { inherit inputs; hostname = systems.x1.hostname; };
+        modules = [
+          ./hosts/x1/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.paul = import ./users/paul/home.nix;
+          }
+        ];
       };
 
-      darwinConfigurations.macbook = mkSystem {
-        hostname = systems.macbook.hostname;
+      darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
         system = systems.macbook.system;
-        pkgs = nixpkgs.legacyPackages.${systems.macbook.system};
-        modules = [ ./hosts/macbook/configuration.nix ];
+        specialArgs = { inherit inputs; hostname = systems.macbook.hostname; };
+        modules = [
+          ./hosts/macbook/configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.paul = import ./users/paul/home.nix;
+          }
+        ];
       };
     };
 }
