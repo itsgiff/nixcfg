25eb68dd2795fa5dc85ad816c7a990bbf97dd022
Paul Gifford <paul@giff.ca>
2025-05-17 16:41:30 -0700

    warbler

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   description = "Unified Nix configurations for x1 (NixOS), macbook (macOS), and nuc (NixOS)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
@@ -22,11 +21,10 @@
     let
       systems = {
         x1 = { hostname = "x1"; system = "x86_64-linux"; username = "paul"; };
-        macbook = { hostname = "macbook"; system = "aarch64-darwin"; username = "paul"; };
         nuc = { hostname = "nuc"; system = "x86_64-linux"; username = "admin"; };
+        macbook = { hostname = "macbook"; system = "aarch64-darwin"; username = "paul"; };
       };
     in {
-      # NixOS configuration for x1
       nixosConfigurations.x1 = nixpkgs.lib.nixosSystem {
         system = systems.x1.system;
         specialArgs = { inherit inputs; hostname = systems.x1.hostname; };
@@ -43,27 +41,23 @@
         ];
       };
 
-      # NixOS configuration for nuc
       nixosConfigurations.nuc = nixpkgs.lib.nixosSystem {
         system = systems.nuc.system;
         specialArgs = { inherit inputs; hostname = systems.nuc.hostname; };
         modules = [
           ./hosts/nuc/configuration.nix
           nixos-hardware.nixosModules.common-cpu-intel
-          # Add other hardware modules as needed
           { nixpkgs.config.allowUnfree = true; }
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = { inherit inputs; hostname = systems.nuc.hostname; };
-            # Point to the correct home.nix file for admin user
             home-manager.users.${systems.nuc.username} = ./users/admin/home.nix;
           }
         ];
       };
 
-      # Darwin configuration
       darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
         system = systems.macbook.system;
         specialArgs = { inherit inputs; hostname = systems.macbook.hostname; };
@@ -82,19 +76,9 @@
         ];
       };
 
-      # Home Manager standalone configurations
       homeConfigurations."paul@x1" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${systems.x1.system};
         extraSpecialArgs = { inherit inputs; hostname = systems.x1.hostname; };
-        modules = [
-          ./users/paul/home.nix
-          { nixpkgs.config.allowUnfree = true; }
-        ];
-      };
-
-      homeConfigurations."paul@macbook" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${systems.macbook.system};
-        extraSpecialArgs = { inherit inputs; hostname = systems.macbook.hostname; };
         modules = [
           ./users/paul/home.nix
           { nixpkgs.config.allowUnfree = true; }
@@ -106,6 +90,15 @@
         extraSpecialArgs = { inherit inputs; hostname = systems.nuc.hostname; };
         modules = [
           ./users/admin/home.nix
+          { nixpkgs.config.allowUnfree = true; }
+        ];
+      };
+
+      homeConfigurations."paul@macbook" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${systems.macbook.system};
+        extraSpecialArgs = { inherit inputs; hostname = systems.macbook.hostname; };
+        modules = [
+          ./users/paul/home.nix
           { nixpkgs.config.allowUnfree = true; }
         ];
       };
