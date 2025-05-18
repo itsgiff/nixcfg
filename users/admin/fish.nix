1f2f6b14f3654c4c3d2e6b91ab0b4cba02e82540
Paul Gifford <paul@giff.ca>
2025-05-17 18:55:06 -0700

    updated and tune home and fish nix files

--- users/admin/fish.nix
+++ users/admin/fish.nix
@@ -1,4 +1,4 @@
-# ~/.nixcfg/users/paul/fish.nix
+# ~/.nixcfg/users/admin/fish.nix
 
 { config, pkgs, lib, ... }:
 
@@ -19,19 +19,27 @@
       c = "clear";  
       g = "git";
 
+      # Docker Managememnt
+      docker-update = "docker-pull; docker-up; docker-clean";
+      docker-up = "sh /mnt/docker/compose.sh up";
+      docker-down = "sh /mnt/docker/compose.sh down";
+      docker-pull = "sh /mnt/docker/compose.sh pull";
+      docker-clean = "docker image prune -af";
+      docker-status = "docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Image}}\t{{.Ports}}'";
+
       # Flake-based configuration aliases
       nixCfg = "cd ~/.nixcfg && $EDITOR flake.nix";
       nixEdit = "cd ~/.nixcfg && $EDITOR";
-      nixSystem = "cd ~/.nixcfg && $EDITOR hosts/x1/configuration.nix";
-      nixHome = "cd ~/.nixcfg && $EDITOR users/paul/home.nix";
+      nixSystem = "cd ~/.nixcfg && $EDITOR hosts/nuc/configuration.nix";
+      nixHome = "cd ~/.nixcfg && $EDITOR users/admin/home.nix";
       
       # Flake building and updating
       nixFlakeCheck = "cd ~/.nixcfg && nix flake check";
       nixFlakeUpdate = "cd ~/.nixcfg && nix flake update";
-      nixSwitch = "cd ~/.nixcfg && sudo nixos-rebuild switch --flake .#x1";
-      homeSwitch = "cd ~/.nixcfg && home-manager switch --flake .#paul@x1";
-      nixUpgrade = "cd ~/.nixcfg && nix flake update && sudo nixos-rebuild switch --flake .#x1";
-      fullUpdate = "cd ~/.nixcfg && nix flake update && sudo nixos-rebuild switch --flake .#x1 && home-manager switch --flake .#paul@x1";
+      nixSwitch = "cd ~/.nixcfg && sudo nixos-rebuild switch --flake .#nuc";
+      homeSwitch = "cd ~/.nixcfg && home-manager switch --flake .#admin@nuc";
+      nixUpgrade = "cd ~/.nixcfg && nix flake update && sudo nixos-rebuild switch --flake .#nuc";
+      fullUpdate = "cd ~/.nixcfg && nix flake update && sudo nixos-rebuild switch --flake .#nuc && home-manager switch --flake .#admin@nuc";
       
       # System maintenance
       nixClean = "sudo nix-collect-garbage --delete-older-than 2d";
