9da2e44fda72da52189ff0a11177407125826e08
Paul Gifford <paul@giff.ca>
2025-05-17 19:09:42 -0700

    updated home.nix for admin

--- users/admin/home.nix
+++ users/admin/home.nix
@@ -1,87 +1,45 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, inputs, hostname, ... }:
 
+let
+  # Map of hostnames to usernames
+  userMapping = {
+    "x1" = "paul";         # ThinkPad
+    "macbook" = "paul";    # Macbook
+    "nuc" = "admin";       # NUC
+    # Add other machines as needed
+  };
+  
+  # Get username based on hostname, defaulting to "paul" if not found
+  username = userMapping.${hostname} or "paul";
+in
 {
+  # Dynamic user information based on hostname
+  home.username = username;
+  home.homeDirectory = 
+    if pkgs.stdenv.isLinux then "/home/${username}" 
+    else if pkgs.stdenv.isDarwin then "/Users/${username}"
+    else "/home/${username}";  # Fallback
+
+  # Application preferences
+  imports = [
+    ../../modules/fastfetch.nix
+    ../../modules/git.nix
+    ../../modules/vscode.nix
+    ./fish.nix
+  ];
+
+    # Home Manager release
+  home.stateVersion = "23.11";
+
+  # Basic packages that don't need dedicated modules
+  home.packages = with pkgs; [
+    htop
+    ripgrep
+  ] ++ (if pkgs ? fastfetch then [ pkgs.fastfetch ] else []);
+
+  # Dotfiles (if needed)
+  home.file = {};
+
   # Let Home Manager install and manage itself
   programs.home-manager.enable = true;
-
-  # Home directory configuration
-  home.username = "admin";
-  home.homeDirectory = "/home/admin";
-  
-  # Fish shell config
-  programs.fish = {
-    enable = true;
-
-    # Fish Aliases
-    shellAliases = {
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      ll = "ls -la";
-      update = "rebuild; docker-pull; docker-up; docker-clean";
-      docker-up = "sh /mnt/docker/compose.sh up";
-      docker-down = "sh /mnt/docker/compose.sh down";
-      docker-pull = "sh /mnt/docker/compose.sh pull";
-      docker-clean = "docker image prune -af";
-      docker-status = "docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Image}}\t{{.Ports}}'";
-      docker-logs = "docker logs -f";
-      rebuild = "sudo nixos-rebuild switch --flake ~/.nixcfg#nuc";
-      infra = "cd /mnt/data/scripts/infrastructure";
-    };
-    
-    functions = {
-      fish_gretting = {
-        description = "Greeting to show when starting a fish shell";
-        body = "";
-      };
-    }; 
-  };
-
-  # Git configuration
-  programs.git = {
-    enable = true;
-    userName = "Paul Gifford";
-    userEmail = "paul@giff.ca";
-    aliases = {
-      st = "status";
-      co = "checkout";
-      ci = "commit";
-      br = "branch";
-      unstage = "reset HEAD --";
-      last = "log -1 HEAD";
-      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-    };
-
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = false;
-      core.editor = "nano";
-      color.ui = true;
-      push.default = "simple";
-    };
-    ignores = [
-      ".DS_Store"
-      "*.swp"
-      "*~"
-      ".idea/"
-      ".vscode/"
-    ];
-  };
-  
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  
-  # Additional packages for the user
-  home.packages = with pkgs; [
-    firefox
-    vscode
-    htop
-    jq
-    bat
-    ripgrep
-    tmux
-    fastfetch
-  ];
-  
-  # Required for home-manager
-  home.stateVersion = "23.11";
 }
