54092772e51f8bfbbb0c3b2d554948f12935fb97
Paul Gifford <paul@giff.ca>
2025-11-06 12:28:28 -0800

    fix: configure NVIDIA container runtime for Docker

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -11,26 +11,7 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
-    daemon.settings = {
-      features = {
-        cdi = true;
-      };
-    };
   };
 
   users.users.admin.extraGroups = [ "docker" "video" "render" ];
-
-  systemd.services.setup-nvidia-cdi = {
-    description = "Generate NVIDIA CDI specs";
-    after = [ "docker.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      mkdir -p /etc/cdi
-      ${pkgs.nvidia-container-toolkit}/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
-    '';
-  };
 }
