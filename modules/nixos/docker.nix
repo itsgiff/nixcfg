75fb4111af54f4f1424ee57939732a337633c1ad
Paul Gifford <paul@giff.ca>
2025-11-06 11:57:43 -0800

    fix: configure NVIDIA container runtime for Docker

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -1,34 +1,26 @@
 { config, pkgs, lib, ... }:
 
 {
-  ########################################
-  ## Docker + NVIDIA on NixOS (stable)
-  ########################################
-
-  # NVIDIA Container Toolkit drivers/hooks
   hardware.nvidia-container-toolkit.enable = true;
 
-  # Make sure the runtime + libs are actually in the system profile
   environment.systemPackages = with pkgs; [
     nvidia-container-toolkit
     libnvidia-container
-    # optional: docker-compose v2 CLI, if you want the standalone too
-    # docker-compose
   ];
 
-  # Docker daemon
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
 
-    # Daemon config with a STABLE runtime path (not a GC'ed /nix/store hash)
     daemon.settings = {
-      default-runtime = "runc";
-      runtimes = {};
+      runtimes = {
+        nvidia = {
+          path = "nvidia-container-runtime";
+          runtimeArgs = [];
+        };
+      };
     };
   };
 
-  # Your user needs the right groups
   users.users.admin.extraGroups = [ "docker" "video" "render" ];
-
 }
